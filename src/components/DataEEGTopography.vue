14b8ec47cdffba7fd2735daf92aeea4ba60032f4
FuXiangyu <michaelf027@outlook.com>
2025-01-05 16:16:55 +0800

    adjust eeg topography styles

--- src/components/DataEEGTopography.vue
+++ src/components/DataEEGTopography.vue
@@ -9,7 +9,15 @@ const store = useStore()
 <template>
   <div id="container">
     <div id="panel">
-      
+      <span id="areaTitle">脑电地形图</span>
+      <div class="rowFlex">
+        <div id="topographyA" class="topography">A</div>
+        <div id="topographyB" class="topography">B</div>
+      </div>
+      <div class="rowFlex">
+        <div id="topographyC" class="topography">C</div>
+        <div id="topographyD" class="topography">D</div>
+      </div>
     </div>
   </div>
 </template>
@@ -24,12 +32,39 @@ const store = useStore()
 }
 
 #panel {
-  width: calc(100% - 40px);
-  height: calc(100% - 40px);
+  width: calc(100% - 20px);
+  height: calc(100% - 20px);
   background: #100b2b;
   border-radius: 5px;
   margin: 5px;
-  padding: 15px;
+  padding: 5px;
+}
+
+#areaTitle {
+  display: block;
+  color: white;
+  font-size: 16px;
+  height: 30px;
+}
+
+.rowFlex {
+  display: flex;
+  justify-content: space-between;
+  height: calc((100% - 30px) * 0.5);
+  width: 100%;
+}
+
+.topography {
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
 }
 
 </style>
